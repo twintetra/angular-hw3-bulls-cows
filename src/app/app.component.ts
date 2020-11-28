acb0c64b11644341873c615a35a2ffbc85e64d42
Andrey Smirnov <twintetra@gmail.com>
2020-11-28 20:12:50 +0300

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,41 @@
 import {Component} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent  {
   value = '';
-  comment = '';
-  computerArr = [];
-  disable = false;
 
-  currentStep = 0;
-  bulls = 0;
-  cows = 0;
-  steps = [];
+  constructor(public appService: AppService) {
+    this.appService.bruteForce();
+  }
 
   valueFromUser(event) {
     this.value = event;
-    this.currentStep += 1;
-    this.comment = '';
-    this.bulls = 0;
-    this.cows = 0;
+    this.appService.setCurrentStep()
+    this.appService.resetComment();
+
+    this.appService.resetBulls();
+    this.appService.resetCows();
     this.checkBullCows();
-    this.steps.push(`Ход ${this.currentStep}: быков ${this.bulls}, коров: ${this.cows}`);
-    this.checkWin();
+    this.appService.pushSteps();
+    this.appService.checkWin();
   }
 
   checkBullCows() {
     const tmpNum = this.value.split('').map(i => Number(i))
     tmpNum.forEach((item, index) => {
-      this.computerArr.forEach((computerItem, computerIndex) => {
+      this.appService.getComputerArr().forEach((computerItem, computerIndex) => {
         if (item === computerItem && index === computerIndex) {
-          this.bulls += 1;
+          this.appService.setBulls();
         } else if (item === computerItem && index !== computerIndex) {
-          this.cows += 1;
+          this.appService.setCows();
         }
       })
     })
-  }
-
-  checkWin() {
-    if (this.bulls === 3) {
-      this.comment = 'Сильный ход!';
-    }
-    if (this.bulls === 4) {
-      this.comment = `Победа!!! Ходов сделано: ${this.currentStep}`;
-      this.disable = true;
-    }
-  }
-
-  bruteForce() {
-    let randomNumber;
-    while (this.computerArr.length < 4) {
-      randomNumber = Math.floor(Math.random() * 10);
-      if (!this.computerArr.includes(randomNumber)) {
-        this.computerArr.push(randomNumber);
-      }
-    }
-  }
-
-  resetGame() {
-    this.computerArr = [];
-    this.currentStep = 0;
-    this.disable = false;
-    this.bruteForce();
-    this.steps = [];
-  }
-
-  constructor() {
-    this.bruteForce();
   }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AppService {
+
+  currentStep = 0;
+  comment = '';
+  disable = false;
+  bulls = 0;
+  cows = 0;
+  steps = [];
+  computerArr = [];
+
+
+  bruteForce() {
+    let randomNumber;
+    while (this.computerArr.length < 4) {
+      randomNumber = Math.floor(Math.random() * 10);
+      if (!this.computerArr.includes(randomNumber)) {
+        this.computerArr.push(randomNumber);
+      }
+    }
+  }
+
+  getComputerArr() {
+    return this.computerArr;
+  }
+
+  setComputerArr() {
+    this.computerArr = [];
+  }
+
+  getBulls() {
+    return this.bulls;
+  }
+  getCows() {
+    return this.cows;
+  }
+  getSteps() {
+    return this.steps;
+  }
+
+  getComment() {
+    return this.comment;
+  }
+
+  getDisable() {
+    return this.disable;
+  }
+
+  setCurrentStep() {
+    this.currentStep += 1;
+  }
+
+  resetComment() {
+    this.comment = '';
+  }
+
+  resetCurrentStep() {
+    this.currentStep = 0;
+  }
+
+  resetBulls() {
+    this.bulls = 0;
+  }
+
+  resetCows() {
+    this.cows = 0;
+  }
+
+  resetSteps() {
+    this.steps = [];
+  }
+
+  pushSteps() {
+    this.steps.push(`Ход ${this.currentStep}: быков ${this.bulls}, коров: ${this.cows}`);
+  }
+
+  setBulls() {
+    this.bulls += 1;
+  }
+
+  setCows() {
+    this.cows += 1;
+  }
+
+  checkWin() {
+    if (this.bulls === 3) {
+      this.comment = 'Сильный ход!';
+    }
+    if (this.bulls === 4) {
+      this.comment = `Победа!!! Ходов сделано: ${this.currentStep}`;
+      this.disable = true;
+    }
+  }
+
+  resetGame() {
+    this.setComputerArr();
+    this.resetCurrentStep();
+    this.disable = false;
+    this.bruteForce();
+    this.resetSteps();
+  }
+}
